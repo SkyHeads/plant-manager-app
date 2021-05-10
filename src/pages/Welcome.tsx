5d0dd228a3fe07275c3d7a819526c1da4b7b60dc
SkyHeads <skyheads@live.com>
2021-05-09 22:27:47 -0300

    initial setup

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Welcome: React.FC = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+}
+
+export default Welcome;
