2967b06cde46a3eb7f9d65a45806f704cb926b4f
SkyHeads <skyheads@live.com>
2021-05-11 00:07:58 -0300

    final welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -3,13 +3,15 @@ import {
   View,
   Text,
   StyleSheet,
-  // Image,
-  // TouchableOpacity,
-  // Dimensions,
+  Image,
+  TouchableOpacity,
+  Dimensions,
   SafeAreaView,
 } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
+import wateringImg from '../assets/watering.png';
 
 const Welcome: React.FC = () => {
   return (
@@ -20,6 +22,17 @@ const Welcome: React.FC = () => {
           suas plantas de {'\n'}
           forma fácil
         </Text>
+
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+
+        <Text style={styles.subTitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -42,6 +55,29 @@ const styles = StyleSheet.create({
     marginTop: 38,
     lineHeight: 34,
     fontFamily: fonts.heading,
+  },
+  subTitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  image: {
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white,
   },
 });
 
