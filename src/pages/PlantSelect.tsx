555686e63b6f789c2a85257670934ccff027db08
SkyHeads <skyheads@live.com>
2021-05-13 16:51:57 -0300

    fetch enviroments to data flatlist

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+import EnviromentButton from '../components/EnviromentButton';
 import Header from '../components/Header';
+import api from '../services/api';
+
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface EnviromentsProps {
+  key: string;
+  title: string;
+}
+
 const PlantSelect: React.FC = () => {
+  const [enviroments, setEnviroments] = useState<EnviromentsProps[]>([]);
+
+  useEffect(() => {
+    async function fetchEnviroment() {
+      const { data } = await api.get('plants_environments');
+      setEnviroments([
+        {
+          key: 'all',
+          title: 'Todos',
+        },
+        ...data,
+      ]);
+    }
+
+    fetchEnviroment();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -12,6 +38,16 @@ const PlantSelect: React.FC = () => {
 
         <Text style={styles.title}>Em qual ambiente</Text>
         <Text style={styles.subTitle}>você quer colocar sua planta?</Text>
+      </View>
+      <View>
+        <FlatList
+          keyExtractor={item => String(item.key)}
+          data={enviroments}
+          renderItem={({ item }) => <EnviromentButton title={item.title} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.enviromentList}
+        />
       </View>
     </View>
   );
@@ -36,7 +72,15 @@ const styles = StyleSheet.create({
     color: colors.heading,
   },
   header: {
-    paddingHorizontal: 30,
+    paddingHorizontal: 32,
+  },
+  enviromentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 28,
+    marginVertical: 32,
+    paddingRight: 32,
   },
 });
 
