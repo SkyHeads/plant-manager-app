1441e88baf9c60a46d83702ee4f4fff21ab1065e
SkyHeads <skyheads@live.com>
2021-05-26 22:50:35 -0300

    create myplants page and add in stack routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,6 +8,7 @@ import Confirmation from '../pages/Confirmation';
 import UserIdentification from '../pages/UserIdentification';
 import PlantSelect from '../pages/PlantSelect';
 import PlantSave from '../pages/PlantSave';
+import MyPlants from '../pages/MyPlants';
 
 const stackRoutes = createStackNavigator();
 
@@ -29,6 +30,7 @@ const AppRoutes: React.FC = () => {
       />
       <stackRoutes.Screen name="PlantSelect" component={PlantSelect} />
       <stackRoutes.Screen name="PlantSave" component={PlantSave} />
+      <stackRoutes.Screen name="MyPlants" component={MyPlants} />
     </stackRoutes.Navigator>
   );
 };
