2a7739a9353df5e239b13e081d65749abcdfcf3f
SkyHeads <skyheads@live.com>
2021-05-26 22:47:03 -0300

    create params for confirmation page

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import Button from '../components/Button';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: 'smile' | 'hug';
+  nextScreen: string;
+}
+
+const emojis = {
+  hug: '🤗',
+  smile: '😄',
+};
+
 const Confirmation: React.FC = () => {
+  const routes = useRoute();
   const navigation = useNavigation();
 
+  const { title, buttonTitle, icon, nextScreen, subtitle } =
+    routes.params as Params;
+
   function handleMoveOn() {
-    navigation.navigate('PlantSelect');
+    navigation.navigate(nextScreen);
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.emoji}>😃</Text>
-        <Text style={styles.title}>Prontinho</Text>
-        <Text style={styles.subTitle}>
-          Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
-        </Text>
+        <Text style={styles.emoji}>{emojis[icon]}</Text>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subTitle}>{subtitle}</Text>
         <View style={styles.footer}>
-          <Button title="Começar" onPress={handleMoveOn} />
+          <Button title={buttonTitle} onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
