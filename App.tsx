090a38a97e500201733167d1f8e278f54e9b184f
SkyHeads <skyheads@live.com>
2021-05-28 01:48:20 -0300

    create notification handler and listener

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
+import * as Notification from 'expo-notifications';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
 } from '@expo-google-fonts/jost';
+import { PlantProps } from './src/lib/storage';
+
 import Routes from './src/routes';
 
 const App: React.FC = () => {
@@ -13,6 +16,35 @@ const App: React.FC = () => {
     Jost_400Regular,
     Jost_600SemiBold,
   });
+
+  useEffect(() => {
+    Notification.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
+    const subscription = Notification.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      },
+    );
+
+    return () => subscription.remove();
+
+    // async function notifications() {
+    //   await Notifications.cancelAllScheduledNotificationsAsync();
+
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("######## NOTIFICAÇÕES AGENDAS ########")
+    //   console.log(data);
+    // }
+
+    // notifications();
+  }, []);
 
   if (!fontsLoaded) {
     return <AppLoading />;
